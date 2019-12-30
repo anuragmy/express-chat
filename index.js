c0004e138cba641453279b236bee537e25a63316
anurag <anuramy2729@gmail.com>
2019-12-31 04:27:46 +0530

    fetching messages

--- index.js
+++ index.js
@@ -3,4 +3,24 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "dist")));
+
+const messages = [
+  {
+    name: "anurag",
+    messgae: "hi"
+  },
+  {
+    name: "anurag1",
+    messgae: "hi1"
+  },
+  {
+    name: "anurag2",
+    messgae: "hi2"
+  }
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
 app.listen(8000, () => console.log("listening..."));
